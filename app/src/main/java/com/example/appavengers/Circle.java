2088f5a9c2ac0f65d0e6b45de53ea176ff98a149
WilliamGuo2002 <yg17@ualberta.ca>
2024-01-30 18:59:29 -0700

    push circle

---
+++ app/src/main/java/com/example/appavengers/Circle.java
@@ -0,0 +1,28 @@
+package com.example.appavengers;
+
+public class Circle extends Shape{
+
+    private Integer x;
+    private Integer y;
+
+    public Circle(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+}
